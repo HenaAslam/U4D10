635ca3b0b70cf222a725b83cdff95169e5fff20f
ajeer333 <ajeer333@gmail.com>
2023-03-10 12:52:42 +0100

    commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,33 @@
 import express from "express";
 import mediasRouter from "./api/medias/index.js";
+import cors from "cors";
 import {
   badRequestHandler,
   unauthorizedHandler,
   notFoundHandler,
   genericErrorHandler,
 } from "./errorHandlers.js";
+
 const server = express();
 const port = process.env.PORT;
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+server.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        corsNext(null, true);
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
+);
+
 server.use(express.json());
 server.use("/medias", mediasRouter);
 
